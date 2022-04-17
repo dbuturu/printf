98f1fa405d2966eacb7459d5da4c23e61c82f8c8
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-04-17 19:18:34 +0300

    Upadated _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -1,10 +1,10 @@
-#include <unistd.h>
+#include "main.h"
 /**
  * _putchar - writes the character c to stdout
  * @c: the character to print
  * Return: 0 on success and -1 error and errno is set appropiately
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
+int _putchar(char c) {
+  write(1, &c, 1);
+  return (1);
 }
