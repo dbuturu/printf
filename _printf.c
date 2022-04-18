ca0edccfaf27b0f3c4d1f75f803344d75e352889
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-04-18 18:48:00 +0300

    Updated _printf

--- _printf.c
+++ _printf.c
@@ -5,6 +5,63 @@
 #define SPECIFIERS_STATE 1
 
 /**
+ * normal_state - Normal state cases.
+ * @format: The input format.
+ * @i: The counter.
+ * @state: The state.
+ * @output_length: The output length.
+ */
+
+void normal_state(const char *format, int i, int *state, int *output_length)
+{
+	switch (format[i])
+	{
+		case '%':
+			(*state) = SPECIFIERS_STATE;
+			break;
+		default:
+			(*output_length) += _putchar(format[i]);
+			break;
+	}
+}
+
+/**
+ * specifiers_state - Specifiers state cases.
+ * @format: The input format.
+ * @args: Other params.
+ * @i: The counter.
+ * @output_length: The output length.
+ * @return: The output length.
+ */
+
+int specifiers_state(
+		const char *format,
+		va_list args,
+		int i,
+		int output_length
+		)
+{
+	switch (format[i])
+	{
+		case 'c':
+			output_length += _putchar((char)va_arg(args, int));
+			break;
+
+		case 's':
+			output_length += _puts(va_arg(args, char *));
+			break;
+
+		case '%':
+			output_length += _putchar('%');
+			break;
+		case 'd':
+		case 'i':
+			break;
+	}
+	return (output_length);
+}
+
+/**
  * _printf - Produces output according to a format.
  * @format: The input format.
  * Return: The length of output.
@@ -23,35 +80,11 @@ int _printf(const char *format, ...)
 		switch (state)
 		{
 			case NORMAL_STATE:
-				switch (format[i])
-				{
-					case '%':
-						state = SPECIFIERS_STATE;
-						break;
-					default:
-						output_length += _putchar(format[i]);
-						break;
-				}
+				normal_state(format, i, &state, &output_length);
 				break;
 
 			case SPECIFIERS_STATE:
-				switch (format[i])
-				{
-					case 'c':
-						output_length += _putchar((char)va_arg(args, int));
-						break;
-
-					case 's':
-						output_length += _puts(va_arg(args, char *));
-						break;
-
-					case '%':
-						output_length += _putchar('%');
-						break;
-					case 'd':
-					case 'i':
-						break;
-				}
+				output_length = specifiers_state(format, args, i, output_length);
 				state = NORMAL_STATE;
 				break;
 		}
