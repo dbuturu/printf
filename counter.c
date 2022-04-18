76cbb937a2b9ef394bc865db54fc2e9c2c4e0981
lindamukami <lindamukami32@gmail.com>
2022-04-18 14:14:16 +0000

    file to count string and main.h

---
+++ counter.c
@@ -0,0 +1,18 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * counter - this function counts the length of strings
+ * @string: The string to count
+ * Return: count of string,
+ */
+
+int counter(char *string)
+{
+	int i = 0;
+
+	for (i = 0; string[i] != '\0'; i++);
+
+	return (i)
+}
